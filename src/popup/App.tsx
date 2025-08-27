7d7f163c1a5a97326b68fc4bd66ebe1a3702a685
Ishan <is9678@srmist.edu.in>
2025-08-27 13:14:58 +0530

    refactor: components directory

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,7 +1,7 @@
 import crxLogo from '@/assets/crx.svg'
 import reactLogo from '@/assets/react.svg'
 import viteLogo from '@/assets/vite.svg'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/popup/components/HelloWorld'
 import './App.css'
 
 export default function App() {
